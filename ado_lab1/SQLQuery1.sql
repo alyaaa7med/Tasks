ea0435c8dcbef225d19be27dc8b6bbcfb1ff006d
unknown <alyaaahmed0643@gmail.com>
2025-10-07 11:30:18 +0300

    add lab1 ado

---
+++ ado_lab1/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿USE MyDB;
+
+CREATE TABLE Department (
+    Id INT PRIMARY KEY IDENTITY(1,1), -- Auto-increment ID
+    Name NVARCHAR(100) NOT NULL       -- Department name
+);
+
+INSERT INTO Department (Name)
+VALUES ('HR'), ('IT'), ('Finance');
+
+
+SELECT * FROM Department;
+
---
+++ ado_lab1/ado_lab1/Program.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace ado_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(
+                "Data Source =.; Initial Catalog = MyDB;" +
+                " Integrated Security = True; Trust Server Certificate = True"
+                );
+
+            //SqlCommand cmd = new SqlCommand("select * from department", con);
+            //con.Open();
+
+            //SqlDataReader reader = cmd.ExecuteReader();
+
+            //while (reader.Read())
+            //{
+            //    Console.WriteLine(reader[0] + "  " + reader[1]);
+            //}
+            //con.Close();
+
+
+            //task : make crud operations 
+            //insert 
+            string insert_sql = "insert into department  (Name) values ('Finance')";
+
+            using (SqlCommand cmd2 = new SqlCommand(insert_sql, con))
+            {
+                con.Open();
+                cmd2.ExecuteNonQuery();
+                Console.WriteLine($"done inserted");
+            }
+
+
+
+            //reterive
+            insert_sql = "select * from department";
+
+            using (SqlCommand cmd2 = new SqlCommand(insert_sql, con))
+            {
+                using (SqlDataReader reader = cmd2.ExecuteReader()) 
+                {
+                    while (reader.Read())
+                    {
+                        int id = (int)reader[0];
+                        string name = (string)reader[1];
+
+                        Console.WriteLine($"Id: {id}, Name: {name}");
+                    }
+                }
+                con.Close(); 
+            }
+
+            //update
+            insert_sql = "update  department set name = 'dot net' where id = 3 ";
+            using (SqlCommand cmd2 = new SqlCommand(insert_sql, con))
+            {
+                con.Open();
+                cmd2.ExecuteNonQuery();
+                Console.WriteLine($"done updated");
+                con.Close();
+
+            }
+
+            //delete
+            insert_sql = "delete from  department where  id = 4";
+            using (SqlCommand cmd2 = new SqlCommand(insert_sql, con))
+            {
+                con.Open();
+                cmd2.ExecuteNonQuery();
+                Console.WriteLine($"done deleted");
+                con.Close();
+
+            }
+
+
+        }
+    }
+}
